14aead8b083a9fda7aadcfc7345e3df1e65522fc
sahilupta <guptasahil81660@gmail.com>
2026-07-31 09:50:35 +0530

    Update saini.py

--- saini.py
+++ saini.py
@@ -278,7 +278,7 @@ async def download_and_decrypt_video(url, cmd, name, key):
 async def send_vid(bot: Client, m: Message, cc, filename, vidwatermark, thumb, name, prog, channel_id):
     subprocess.run(f'ffmpeg -i "{filename}" -ss 00:00:10 -vframes 1 "{filename}.jpg"', shell=True)
     await prog.delete (True)
-    reply1 = await bot.send_message(channel_id, f"**📩 Uploading Video 📩:-**\n<blockquote>**{✧𝒮𝒶𝒽𝒾𝓁 ✧ }**</blockquote>")
+    reply1 = await bot.send_message(channel_id, f"**📩 Uploading Video 📩:-**\n<blockquote>**{𝒮𝒶𝒽𝒾𝓁}**</blockquote>")
     reply = await m.reply_text(f"**Generate Thumbnail:**\n<blockquote>**{name}**</blockquote>")
     try:
         if thumb == "/d":
